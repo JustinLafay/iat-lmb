57f360c29517b1790feaab2614b0729d7f15eac6
JustinLafay <justin.lafay@insa-lyon.fr>
2023-07-02 18:11:08 +0200

    ajout des données de score dans un fichier test_scores.txt

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dqn_agent import DQNAgent
 from epsilon_profile import EpsilonProfile
 from game.SpaceInvaders import SpaceInvaders
 import game
+import numpy as np
 
 from networks import MLP, CNN
 
@@ -36,7 +37,7 @@ def test_maze(env: SpaceInvaders, agent: DQNAgent, max_steps: int, nepisodes : i
 def main(nn: str= "mlp", mode : str= "test"):
     
     if(mode == "learn"):
-        env = SpaceInvaders(display=False)
+        env = SpaceInvaders(display=True)
     if(mode == "test"):
         env = SpaceInvaders(display=True)
     """ INSTANCIE LE LABYRINTHE """ 
@@ -44,16 +45,16 @@ def main(nn: str= "mlp", mode : str= "test"):
 
     """ INITIALISE LES PARAMETRES D'APPRENTISSAGE """
     # Hyperparamètres basiques
-    n_episodes = 25000
-    max_steps = 1000
-    gamma = 1.
-    alpha = 0.01
+    n_episodes = 100
+    max_steps = 5
+    gamma = 0.05
+    alpha = 0.5
     eps_profile = EpsilonProfile(1.0, 0.1)
 
     # Hyperparamètres de DQN
-    final_exploration_episode = 8333
-    batch_size = 64
-    replay_memory_size = 1000
+    final_exploration_episode = 30
+    batch_size = len(env.get_state())
+    replay_memory_size = 100
     target_update_frequency = 100
     tau = 1.0
 
@@ -75,7 +76,10 @@ def main(nn: str= "mlp", mode : str= "test"):
     agent = DQNAgent(model, eps_profile, gamma, alpha, replay_memory_size, batch_size, target_update_frequency, tau, final_exploration_episode)
     print("******* mode = "+mode+"********")
     if(mode == "learn"):
-        agent.learn(env, n_episodes, max_steps)
+        lines = agent.learn(env, n_episodes, max_steps)
+        with open('test_scores.txt', 'w') as f:
+            for line in lines:
+                f.write(f"{line}\n")
         agent.save_qfunction()
     elif(mode == "test"):
         print("Début du test")
